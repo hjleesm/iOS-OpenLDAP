3ca8eb43a8654d97a27f9e00bb227b38dec95686
hjleesm <hjleesm@naver.com>
2020-09-16 11:09:45 +0900

    Initial Commit

---
+++ iOS-OpenLDAP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-OpenLDAP
+//
+//  Created by hjlee on 2020/09/16.
+//  Copyright © 2020 yourcompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
